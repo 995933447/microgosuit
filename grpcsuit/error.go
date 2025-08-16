4584b4c747a0d08638bc5c3e22aa066fd440d4ee
995933447 <34621597+995933447@users.noreply.github.com>
2025-08-16 17:14:39 +0200

    Update error.go

--- grpcsuit/error.go
+++ grpcsuit/error.go
@@ -6,27 +6,15 @@ import (
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
-func NewErrFromEnumWithMsg(err protoreflect.Enum, errMsg string) error {
+func NewRpcErrWithMsg(err protoreflect.Enum, errMsg string) error {
 	if errMsg == "" {
 		errMsg = string(err.Descriptor().Values().ByNumber(err.Number()).Name())
 	}
 	return status.Errorf(codes.Code(err.Number()), errMsg)
 }
 
-func NewErrFromEnum(err protoreflect.Enum) error {
-	return NewErrFromEnumWithMsg(err, "")
-}
-
 func NewRpcErr(err protoreflect.Enum) error {
-	return NewErrFromEnum(err)
-}
-
-func NewRpcErrWithMsg(err protoreflect.Enum, msg string) error {
-	return NewErrFromEnumWithMsg(err, msg)
-}
-
-func IsInternalError(err error) bool {
-	return IsUnknownError(err)
+	return NewRpcErrWithMsg(err, "")
 }
 
 func IsUnknownError(err error) bool {

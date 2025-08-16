37c8ddb05a01ff3d4807091ee05cbe056b484640
995933447 <34621597+995933447@users.noreply.github.com>
2025-08-16 17:12:16 +0200

    Update error.go

--- grpcsuit/error.go
+++ grpcsuit/error.go
@@ -25,6 +25,15 @@ func NewRpcErrWithMsg(err protoreflect.Enum, msg string) error {
 	return NewErrFromEnumWithMsg(err, msg)
 }
 
+func IsInternalError(err error) bool {
+	return IsUnknownError(err)
+}
+
+func IsUnknownError(err error) bool {
+	code := GetRpcErrCode(err)
+	return code == int32(codes.Unknown) || code == -1
+}
+
 func GetRpcErrCode(err error) int32 {
 	st, ok := status.FromError(err)
 	if ok {

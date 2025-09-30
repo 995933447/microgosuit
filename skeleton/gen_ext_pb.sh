cff1921fd8c71c6c13b5d430f174566a923bdf1c
995933447 <34621597+995933447@users.noreply.github.com>
2025-09-30 17:26:39 +0200

    Update gen_ext_pb.sh

--- skeleton/gen_ext_pb.sh
+++ skeleton/gen_ext_pb.sh
@@ -1,2 +1,2 @@
 mkdir -p ./pb
-protoc --go_out=./pb --go-grpc_out=. --go_opt=paths=source_relative --proto_path=./proto ./proto/ext.proto
+protoc --go_out=./pb --go_opt=paths=source_relative --proto_path=./proto ./proto/ext.proto

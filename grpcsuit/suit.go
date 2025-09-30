5896b1ea283e1b61a0383bc084709e64aba63e2d
995933447 <34621597+995933447@users.noreply.github.com>
2025-09-30 09:33:47 +0200

    Update suit.go

--- grpcsuit/suit.go
+++ grpcsuit/suit.go
@@ -3,6 +3,7 @@ package grpcsuit
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/995933447/microgosuit/discovery"
 	"google.golang.org/grpc"
@@ -18,6 +19,39 @@ var RoundRobinDialOpts = []grpc.DialOption{
 
 var NotRoundRobinDialOpts = []grpc.DialOption{
 	grpc.WithTransportCredentials(insecure.NewCredentials()),
+}
+
+var (
+	customizedDialOpts                  []grpc.DialOption
+	customizedDialOptsMergedDefault     []grpc.DialOption
+	customizedDialOptsMergedDefaultOnce sync.Once
+)
+
+func RegisterCustomizedDialOpts(opts ...grpc.DialOption) {
+	customizedDialOpts = append(customizedDialOpts, opts...)
+	customizedDialOptsMergedDefault = nil
+}
+
+func GetCustomizedOpts() []grpc.DialOption {
+	return customizedDialOpts
+}
+
+func GetCustomizedOptsDefault() []grpc.DialOption {
+	if len(customizedDialOpts) == 0 {
+		return RoundRobinDialOpts
+	}
+	return customizedDialOpts
+}
+
+func GetCustomizedOptsMergedDefault() []grpc.DialOption {
+	if len(customizedDialOptsMergedDefault) == 0 {
+		customizedDialOptsMergedDefaultOnce.Do(func() {
+			customizedDialOptsMergedDefault = make([]grpc.DialOption, len(RoundRobinDialOpts))
+			copy(customizedDialOptsMergedDefault, RoundRobinDialOpts)
+			customizedDialOptsMergedDefault = append(customizedDialOptsMergedDefault, customizedDialOpts...)
+		})
+	}
+	return customizedDialOptsMergedDefault
 }
 
 var customDoOnDiscoverSrvUpdated discovery.OnSrvUpdatedFunc = func(ctx context.Context, evt discovery.Evt, srv *discovery.Service) {}

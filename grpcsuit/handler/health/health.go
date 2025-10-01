73f3686d8e437b6a4155fa34b57acbc5cb5a3a50
995933447 <34621597+995933447@users.noreply.github.com>
2025-10-01 09:55:06 +0200

    Update health.go

--- grpcsuit/handler/health/health.go
+++ grpcsuit/handler/health/health.go
@@ -6,10 +6,25 @@ import (
 
 type Reporter struct {
 	UnimplementedHealthReporterServer
+	serviceNames map[string]struct{}
 }
 
-func (h *Reporter) Ping(_ context.Context, _ *PingReq) (*PingResp, error) {
-	return &PingResp{}, nil
+func NewReporter(serviceNames []string) *Reporter {
+	r := &Reporter{
+		serviceNames: make(map[string]struct{}),
+	}
+	for _, serviceName := range serviceNames {
+		r.serviceNames[serviceName] = struct{}{}
+	}
+	return r
+}
+
+func (r *Reporter) Ping(_ context.Context, req *PingReq) (*PingResp, error) {
+	var resp PingResp
+	if r.serviceNames != nil {
+		_, resp.Ok = r.serviceNames[req.PingService]
+	}
+	return &resp, nil
 }
 
 var _ HealthReporterServer = (*Reporter)(nil)
